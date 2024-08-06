bf27e3376db10cc0d809b1b2f64858a709e2f6ea
Bandla Guruprasad <131840350+BANDLA-BOT@users.noreply.github.com>
2024-08-06 19:03:56 +0530

    started working on backend

--- server/dist/index.dev.js
+++ server/dist/index.dev.js
@@ -2,7 +2,12 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+_dotenv["default"].config();
+
 var app = (0, _express["default"])();
-//# sourceMappingURL=server.dev.js.map
+var PORT = process.env.PORT || 8000;
+//# sourceMappingURL=index.dev.js.map
---
+++ server/models/contactUsForm.js
@@ -0,0 +1,20 @@
+import mongoose, { model } from 'mongoose'
+
+const contactSchema = new mongoose.Schema({
+    fname:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true
+    },
+    message:{
+        type:String,
+        required:true
+    }
+})
+
+const contactModel = mongoose.model('contactform', contactSchema)
+export default contactModel
